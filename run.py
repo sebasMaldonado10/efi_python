201f3f06cea2509d0e95e3f5f9bf6e2bad04dfc2
sebastian <s.maldonado@itecriocuarto.org.ar>
2025-10-24 00:48:39 -0300

    chore: agregar .gitignore y limpiar pycache

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
 
-app = create_app()
+app = create_app() #funcion create_app
+
+from app.models import Usuario, Post, Comentario, Categoria
 
 if __name__ == '__main__':
     app.run(debug=True) #activamos el modo debug 
